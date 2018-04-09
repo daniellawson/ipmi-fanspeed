53cf2b4fc92fb720cbbd193b8166a8ca0ca86bfa
Daniel Lawson <github@element.meta.net.nz>
2018-04-09 16:36:20 +1200

    Adding usage, script always calls getfanspeed by default

--- ipmi-fanspeed.sh
+++ ipmi-fanspeed.sh
@@ -26,6 +26,7 @@ getfanspeed() {
 	else 
 		echo "Unknown response code \"$SPEED\" ";
 	fi
+	exit 0;
 }
 
 setfanpwm() {
@@ -40,7 +41,6 @@ setfanpwm() {
 	sleep 1
 	res=$(ipmitool raw 0x30 0x70 0x66 0x01 0x00 "$PWM" )
 	sleep 1
-	getfanspeed
 }
 
 setfanspeed() {
@@ -51,24 +51,55 @@ setfanspeed() {
 	fi
 
 	res=$(ipmitool raw 0x30 0x45 0x01 "$SPEED" )
-	getfanspeed
 }
 
-TEMP=$(getopt -o hfqgp: --long heavyio,fullspeed,quiet,get,pwm: -n 'fanspeed.sh' -- "$@")
+usage() {
+  echo -n "ipmi-fanspeed.sh [OPTION]...
+
+Helper script for setting system fanspeeds on an IPMI controller.
+Verified working on various recent Supermicro motherboards, eg X10SDV 
+
+To run this you need to have read/write permissions on your IPMI device 
+(normally /dev/ipmi0, but possibly something else). Usually this means
+you need to run it as root
+
+Note that this makes use of some IPMI RAW commands, so may or may not 
+work on your platform. The profiles in particular may change, so feel free 
+to edit them.
+
+If your IPMI controller supports setting fan profiles via it's web interface, 
+you can try setting a profile there and then running the IPMI raw command
+used in the get function, which should give you the hex code for that mode.
+
+ Options:
+  -g, --get		Get current fan mode, and possibly PWM setings
+  -f, --fullspeed	Set fan to fullspeed
+  -h, --heavyio		Set fan to "Heavy IO" profile
+  -q, --quiet		Set fan to "Quiet" profile
+  -p, --pwm <%age>	Set PWM duty cycle to %age
+      --help		Display usage text and ext
+
+"
+exit 0
+}
+
+TEMP=$(getopt -o hfqgp: --long heavyio,fullspeed,quiet,get,pwm:,help -n 'ipmi-fanspeed.sh' -- "$@")
 eval set -- "$TEMP"
 
 while true; do
-    case "$1" in
+    case "${1:-}" in
+	--help) usage; shift ;;
         -h|--heavyio) setfanspeed $HEAVYIO ; shift ;; 
         -f|--fullspeed) setfanspeed $FULLSPEED ; shift ;;
         -q|--quiet) setfanspeed $QUIET ; shift ;;
         -g|--get) getfanspeed ; shift ;;
 	-p|--pwm) setfanpwm $2 ; shift 2 ;;
         --) shift; break ;;
-        *) echo "Internal error!" ; exit 1;;
+        *) usage; exit ;;
     esac
 done
 
-
+# always call getfanspeed
+getfanspeed
 
 
